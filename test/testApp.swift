dddcf02372cc3ab50582522cd564271704a00c1e
p.m.student <wramirez@students.tulsatech.edu>
2021-01-20 13:54:43 -0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
